0f57aae38bc86b4540ad91efb768ce0e30da9344
jeanfimollheger <jpmh@gmx.fr>
2025-04-15 13:53:05 +0200

    fixed lot of little bugs

--- members/models.py
+++ members/models.py
@@ -1,11 +1,26 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.utils.text import slugify
 
 # Create your models here.
 class Member(AbstractUser):
     # Additional fields can be added here
     bio = models.TextField(blank=True, null=True)
     profile_picture = models.ImageField(upload_to='profile_pictures/', blank=True, null=True)
+    slug = models.SlugField(max_length=150, unique=True, blank=True, null=True)
+
 
     def __str__(self):
         return self.username
+
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.username)
+        super().save(*args, **kwargs)
+
+
+    def get_absolute_url(self):
+        return f"/members/{self.slug}/"
+
+    
--- members/views.py
+++ members/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Member
 
 # Create your views here.
+class MemberListView(ListView):
+    model = Member
+    template_name = 'members/member_list.html'
+    context_object_name = 'members'
+    #paginate_by = 10   #Number of members per page
+
