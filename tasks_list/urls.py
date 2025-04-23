faf5162a289736fbfc2dd99567efc9d6a6d6215a
jeanfimollheger <jpmh@gmx.fr>
2025-04-23 19:37:35 +0200

    admin about Category & Project & but fixed about task_form.html

--- tasks_list/urls.py
+++ tasks_list/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import TaskCreateView
 
 urlpatterns = [
-    path('tasks/create/', TaskCreateView.as_view(), name='task_create'),
+    path('create/', TaskCreateView.as_view(), name='task_create'),
     #path('', TaskListView.as_view(), name='task_list'),
     #path('task/<slug:slug>/', TaskDetailView.as_view(), name='task_detail'),
     #path('task/<slug:slug>/update/', TaskUpdateView.as_view(), name='task_update'),
